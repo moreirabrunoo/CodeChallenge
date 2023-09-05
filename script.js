27e157400a5f9bbc8f3260a0d0351a98529afe1f
moreirabrunoo <bm80150@gmail.com>
2023-09-04 21:58:12 -0300

    commit entrega

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById('registroForm').addEventListener('submit', function (e) {
+    e.preventDefault(); 
+
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const fechaNacimiento = document.getElementById('fechaNacimiento').value;
+
+    const data = {
+        nombre: nombre,
+        apellido: apellido,
+        fechaNacimiento: fechaNacimiento
+    };
+
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .then(data => {
+       document.getElementById('respuesta').innerHTML = `Respuesta del servidor: ${JSON.stringify(data)}`;
+    })
+    .catch(error => {
+        console.error('Error al enviar la solicitud:', error);
+    });
+});
